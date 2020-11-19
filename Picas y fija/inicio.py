0066c8ebeb5ae66a7b162f47e8502c1f6dd0978b
darkelian <55606505+darkelian@users.noreply.github.com>
2020-11-19 14:49:54 -0500

    Update inicio.py

--- Picas y fija/inicio.py
+++ Picas y fija/inicio.py
@@ -1,30 +1,25 @@
 import os
- 
-def menu():
-	"""
-	Función que limpia la pantalla y muestra nuevamente el menu
-	"""
-	os.system('cls') # NOTA para windows tienes que cambiar clear por cls
-	print ("Selecciona una opción")
-	print ("\t1 - Jugar")
-	print ("\t2 - Campeones")
-	print ("\t3 - Salir")
-	
- 
- 
-while True:
-	# Mostramos el menu
-	menu()
- 
-	# solicituamos una opción al usuario
-	opcionMenu = input("Ingresa una opcion >> ")
- 
-	if opcionMenu=="1":
-		from jugar import jugar 
-	elif opcionMenu=="2":
-		from jugador import jugador
-	elif opcionMenu=="3":
-		break
-	else:
-		print ("")
-		input("Opcion incorrecta :(...\npulsa una tecla para continuar")
+class inicio:
+    
+    def menu():
+        #os.system('cls') # NOTA para windows tienes que cambiar clear por cls
+        print("Selecciona una opción")
+        print("\t1 - Jugar")
+        print("\t2 - Campeones")
+        print("\t3 - Salir")
+
+    while True:
+        # Mostramos el menu
+        menu()
+
+    # solicituamos una opción al usuario
+        opcionMenu = input("Ingresa una opcion >> ")
+        if opcionMenu == "1":
+            from jugar import jugar
+        elif opcionMenu == "2":
+            from jugador import jugador  
+        elif opcionMenu == "3":
+            break
+        else:
+            print("")
+            input("Opcion incorrecta :(...\npulsa una tecla para continuar")
